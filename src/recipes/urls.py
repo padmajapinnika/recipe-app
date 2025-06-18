83576502c6ef9f5708af439303a954035b2db19f
padmajapinnika <padmajapinnika@gmail.com>
2025-06-18 09:19:46 -0400

    added login and logout pages

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import home, recipe_list, recipe_detail
+from .views import home, recipe_list, recipe_detail 
+from recipe_project.views import login_view, logout_view
 
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', home, name='home'),          # Welcome page
+    path('', home, name='home'),  
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
     path('list/', recipe_list, name='recipe_list'),  # Recipes overview page
     path('detail/<int:pk>/', recipe_detail, name='recipe_detail'),
 ]
--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Recipe
-
+@login_required(login_url='login')
 def home(request):
     return render(request, 'recipes/recipes_home.html')
 
+@login_required(login_url='login')
 def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
 
+@login_required(login_url='login')
 def recipe_detail(request, pk):
     recipe = get_object_or_404(Recipe, pk=pk)
-    # No need for extra query if related_name is set properly
     return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
